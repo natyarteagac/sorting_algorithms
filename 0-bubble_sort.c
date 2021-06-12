a5fadc4ec34a455366a2fb3400e90c8dd93468f3
helectron <helena.cortes@udea.edu.co>
2021-06-12 15:08:45 -0500

    sort an array with bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sort an array with bubble sort algorithm
+ * @array: Array that is going to  be sorted
+ * @size: size of array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    unsigned int i, j;
+    int tmp;
+    
+    /*Validation*/
+    if (array == NULL || size == 0)
+        return;
+
+    for (i = 0; i < size-1; i++)
+    {
+        for (j = 0; j < size-1; j++)
+        {
+            if(array[j] > array[j+1])
+            {
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
+}
