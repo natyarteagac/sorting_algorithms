34843e3ecf4521b470710dec958a86d607fa8ea4
helectron <helena.cortes@udea.edu.co>
2021-06-13 10:45:20 -0500

    betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,24 +8,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j;
-    int tmp;
-    
-    /*Validation*/
-    if (array == NULL || size == 0)
-        return;
+	unsigned int i, j;
+	int tmp;
 
-    for (i = 0; i < size-1; i++)
-    {
-        for (j = 0; j < size-1; j++)
-        {
-            if(array[j] > array[j+1])
-            {
-                tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	/*Validation*/
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
