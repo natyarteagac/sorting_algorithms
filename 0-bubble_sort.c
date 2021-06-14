aa980d21682775ce2d0cc21023d7bb18d4c27065
helectron <helena.cortes@udea.edu.co>
2021-06-13 20:21:43 -0500

    original function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
