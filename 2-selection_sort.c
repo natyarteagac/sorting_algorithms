a15c86be59733723f8e1e443ba65aab60067745f
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-14 14:33:21 -0500

    Selection sort taks

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort an array with the Selection sort.
+ * @array: Array to sort.
+ * @size: Size of the array.
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j;
+	int min_indx, tmp;
+
+	/* Go through the array one by one*/
+	for (i = 0; i < size - 1; i++)
+	{
+		/* Finding the minimun element in a unsorted array*/
+		min_indx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_indx])
+				min_indx = j;
+		}
+		/* Swap the minimun element with the first element*/
+		if (array[min_indx] < array[i])
+		{
+			tmp = array[min_indx];
+			array[min_indx] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,13 +12,14 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 /*Prototypes*/
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif
