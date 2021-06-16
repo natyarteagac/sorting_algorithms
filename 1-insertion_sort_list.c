62d4a4943698b80acac7cca758705eba231262cb
helectron <helena.cortes@udea.edu.co>
2021-06-16 17:35:53 -0500

    more and more verifications

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
 
-	if (*list == NULL && list == NULL)
+	if (*list == NULL && list == NULL && (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
