8f2c3295a66b93f07dbd543929cf69932c20e6a9
helectron <helena.cortes@udea.edu.co>
2021-06-16 17:20:05 -0500

    yahuaaaaagit add 1-insertion_sort_list.c !

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - 
+ *
+ * 
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current;
+
+    if (!*list)
+        return;
+    
+    current = (*list)->next;
+    while(current)
+    {
+        while(current->prev && current->prev->n > current->n)
+        {
+            current->prev->next = current->next;
+            if (current->next)
+                current->next->prev = current->prev;
+            
+            current->next = current->prev;
+            current->prev = current->next->prev;
+            current->next->prev = current;
+
+            if(!current->prev)
+                *list = current;
+            else
+                current->prev->next = current;
+            print_list(*list);
+        }
+        current = current->next;
+    }
+}
